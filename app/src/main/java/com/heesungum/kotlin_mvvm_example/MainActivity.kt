ad2ca6bab225305f2f1013c7edffe53adfed20a4
HeeSungUm <50452867+HeeSungUm@users.noreply.github.com>
2021-07-23 21:50:00 +0900

    MainActivity.kt 추가

--- app/src/main/java/com/heesungum/kotlin_mvvm_example/MainActivity.kt
+++ app/src/main/java/com/heesungum/kotlin_mvvm_example/MainActivity.kt
@@ -2,10 +2,21 @@ package com.heesungum.kotlin_mvvm_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var contactViewModel: ContactViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        contactViewModel = ViewModelProvider(this).get(ContactViewModel::class.java)
+        contactViewModel.getAll().observe(this, {
+
+        })
+
     }
 }
