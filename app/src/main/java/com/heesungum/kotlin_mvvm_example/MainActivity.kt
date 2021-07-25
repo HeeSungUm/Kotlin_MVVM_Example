efdb57d50026ee0df7ee2cd0c077227716012aee
HeeSungUm <50452867+HeeSungUm@users.noreply.github.com>
2021-07-25 16:05:30 +0900

    rv code 추가

--- app/src/main/java/com/heesungum/kotlin_mvvm_example/MainActivity.kt
+++ app/src/main/java/com/heesungum/kotlin_mvvm_example/MainActivity.kt
@@ -2,8 +2,11 @@ package com.heesungum.kotlin_mvvm_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,11 +15,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val adapter = ContactRvAdapter({contact ->  }, {contact -> deleteDialog(contact)  })
+
+        val lm = LinearLayoutManager(this)
+        main_rv.adapter = adapter
+        main_rv.layoutManager = lm
+        main_rv.setHasFixedSize(true)
 
         contactViewModel = ViewModelProvider(this).get(ContactViewModel::class.java)
         contactViewModel.getAll().observe(this, {
-
+            adapter.setContacts(it)
         })
 
+    }
+
+    private fun deleteDialog(contact: Contact) {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Delete selected contact?")
+            .setNegativeButton("NO") {_, _ ->}
+            .setPositiveButton("YES") {_, _ ->
+                contactViewModel.delete(contact)
+            }
+        builder.show()
     }
 }
